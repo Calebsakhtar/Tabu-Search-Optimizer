6c122ddb04c2dc024534910415c28b8f2f7ef60c
Calebsakhtar <calebsakhtar@gmail.com>
2022-01-27 00:11:02 +0000

    Fix a bug in the Config class header

--- TabuSearch/headers/Config.h
+++ TabuSearch/headers/Config.h
@@ -14,7 +14,7 @@ namespace TS {
 
 	class Config {
 		std::vector<Variable> m_vars; // Stores the ranges of feasible region
-		std::vector<MDR::Design> m_performances; // Store the performance metrics
+		MDR::Design m_performances; // Store the performance metrics
 												 // of the configuration
 		bool m_perfs_evaluated = false; // Stores whether the performances have
 										// been evaluated or not
@@ -28,18 +28,23 @@ namespace TS {
 
 		// Intended constructor (with performance metrics)
 		Config(const std::vector<Variable>& vars,
-			const std::vector<MDR::Design>& performances) {
+			const MDR::Design& performances) {
 			m_vars = vars;
 			m_performances = performances;
+			m_perfs_evaluated = true;
 		};
 
 		// Copy constructor (Currently Disabled)
 		//Config(const Config& ip_var) {};
 
 		// Set the performance metrics
-		void set_performances(const std::vector<MDR::Design>& performances) {
+		void set_performances(const MDR::Design& performances) {
 			m_performances = performances;
+			m_perfs_evaluated = true;
 		};
+
+		// Get the performance metrics
+		MDR::Design get_performances() const { return m_performances; };
 
 		// Return the size of the variables
 		size_t size_vars() const { return m_vars.size(); };
@@ -48,9 +53,9 @@ namespace TS {
 		Variable operator [](int i) const { return m_vars[i]; }
 		Variable& operator [](int i) { return m_vars[i]; }
 
-		// Make a new index operator for performances
-		MDR::Design operator ()(int i) const { return m_performances[i]; }
-		MDR::Design& operator ()(int i) { return m_performances[i]; }
+		//// Overload the index operator
+		//Variable operator [](int i) const { return m_vars[i]; }
+		//Variable& operator [](int i) { return m_vars[i]; }
 	};
 
 }
