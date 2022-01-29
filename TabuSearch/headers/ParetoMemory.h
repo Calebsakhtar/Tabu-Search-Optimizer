054ed48d6da455744be89cd3a9bd2b6a068942db
Calebsakhtar <calebsakhtar@gmail.com>
2022-01-29 13:59:38 +0000

    Improve the functionality of ParetoMemory

--- TabuSearch/headers/ParetoMemory.h
+++ TabuSearch/headers/ParetoMemory.h
@@ -12,19 +12,34 @@ namespace TS {
 
 	class ParetoMemory {
 		std::vector<Config> m_configs; // Stores the ranges of feasible region
+		std::vector<MDR::DomRel> m_dom_rels; // Stores the dominance relations
 
 	public:
 		// Default constructor (constructs an empty object)
 		ParetoMemory() {};
 
 		// Intended constructor
-		ParetoMemory(const Config& sample_config);
+		ParetoMemory(const std::vector<Config>& configs,
+			const std::vector<MDR::DomRel>& dom_rels);
 
 		// Copy constructor (Currently Disabled)
 		//ParetoMemory(const ParetoMemory& ip_var) {};
 
+		// Get a list of the performances
+		std::vector<MDR::Design> get_perf_vect() const;
+
+		// Set the performances from an input vector of performances
+		void set_perf_vect(const std::vector<MDR::Design>& ip_vect);
+
 		// Replace the oldest configuration of the ParetoMemory with the new point visited
 		bool consider_config(const Config& new_config);
+
+		// Add a new configuration to the pareto memory and update the ranks of every config
+		void add_config_update_ranks(const Config& new_config);
+
+		// Overload the index operator
+		Config operator [](int i) const { return m_configs[i]; };
+		Config& operator [](int i) { return m_configs[i]; };
 	};
 
 }
--- TabuSearch/src/ParetoMemory.cpp
+++ TabuSearch/src/ParetoMemory.cpp
@@ -7,16 +7,38 @@
 namespace TS {
 
 	// Intended constructor for the Variable class
-	ParetoMemory::ParetoMemory(const Config& sample_config) {
-
-		std::vector<Config> configs = { sample_config };
+	ParetoMemory::ParetoMemory(const std::vector<Config>& configs,
+		const std::vector<MDR::DomRel>& dom_rels) {
 
 		m_configs = configs;
+		m_dom_rels = dom_rels;
 	}
 
-	// Replace the oldest configuration of the ParetoMemory with the new point visited
-	bool ParetoMemory::consider_config(const Config& new_config) {
+	// Get a list of the performances
+	std::vector<MDR::Design> ParetoMemory::get_perf_vect() const {
+		std::vector<MDR::Design> design_vect;
+
+		for (size_t i = 0; i < m_configs.size(); i++) {
+			design_vect.push_back(m_configs[i].get_performances());
+		}
+
+		return design_vect;
+	}
+
+	// Set the performances from an input vector of performances
+	void ParetoMemory::set_perf_vect(const std::vector<MDR::Design>& ip_vect) {
 		
+		// Ensure the vector is the right length to protect against indexing out of range
+		assert(m_configs.size() == ip_vect.size());
+
+		for (size_t i = 0; i < m_configs.size(); i++) {
+			m_configs[i].set_performances(ip_vect[i]);
+		}
+	}
+
+	// Replace the oldest configurationw of the ParetoMemory with the new point visited if this
+	// new point dominates the configurations
+	bool ParetoMemory::consider_config(const Config& new_config) {
 		
 		for (size_t i = 0; i < m_configs.size(); i++) {
 			Config current_config = m_configs[i];
@@ -24,10 +46,10 @@ namespace TS {
 			MDR::Design ip_design = new_config.get_performances();
 
 			// Check that no members of the ParetoMemory dominate the new config
-			if (MDR::A_dominates_B(current_design, ip_design)) {
+			if (MDR::A_dominates_B_2D(current_design, ip_design)) {
 				return false;
 			}
-			else if (MDR::A_dominates_B(ip_design, current_design)) {
+			else if (MDR::A_dominates_B_2D(ip_design, current_design)) {
 				// If the input dominates any of the existing designs, remove the existing design
 				m_configs.erase(m_configs.begin() + i);
 			}
@@ -40,4 +62,17 @@ namespace TS {
 		return true;
 	}
 
+	void ParetoMemory::add_config_update_ranks(const Config& new_config) {
+		
+		// Get the relevant design performances
+		MDR::Design new_design = new_config.get_performances();
+		std::vector<MDR::Design> existing_designs = get_perf_vect();
+
+		// Update the ranks of the existing designs due to the new design
+		MDR::update_ranks(new_design, existing_designs, m_dom_rels);
+
+		// Add the new design to the existing designs and update the ParetoMemory
+		existing_designs.push_back(new_design);
+		set_perf_vect(existing_designs);
+	};
 }
