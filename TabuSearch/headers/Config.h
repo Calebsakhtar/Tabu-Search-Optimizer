6d155c1bd3cd9ce474249129c060b3017edb1891
Calebsakhtar <46966291+Calebsakhtar@users.noreply.github.com>
2022-01-30 14:35:28 +0000

    Create Base Classes for MDRTS Optimizer (#4)
    * Add the header and source files for Variable
    
    * Update the MDR submodule
    
    * Add the Variable class
    
    * Add logical operator overload to Variable
    
    * Add a test for the overloaded less-than operator
    
    * Add a type alias for vectors of Variables
    
    * Add a test for the Config type alias
    
    * Add a way to check if two variables are the same
    
    * Improve the same_variable functions
    
    * Add the STM class
    
    * Add tests for the STM class
    
    * Change config to make it more useful
    
    * Fix a bug in the Config class header
    
    * Add the Intensification Memory class
    
    * Change IM class name to ParetoMemory
    
    * Update the MDR-Test submodule
    
    * Update the MDR-Test submodule for bugfixing
    
    * Add a function that returns the ranks from Config
    
    * Improve the functionality of ParetoMemory
    
    * Add a way to extract all variables from Config
    
    * Improve the Variable class by using arrays
    
    * Fix a bug in Variable
    
    * Fix the is_feasible Variable function
    
    * Add the LTM class and a diversification function
    
    * Add a src file for Config and is_feasible
    
    * Add the file required to build the project

---
+++ TabuSearch/headers/Config.h
@@ -0,0 +1,65 @@
+#ifndef TS_CONFIGURATION_H
+#define TS_CONFIGURATION_H
+
+#include <vector>
+#include <string>
+
+#include "Variable.h"
+#include "../../MDR-Test/MDR Test Project/headers/DesignClasses.h"
+#include "../../MDR-Test/MDR Test Project/headers/MDRFunctions.h"
+
+namespace TS {
+
+	//using Config = std::vector<Variable>;
+
+	class Config {
+		std::vector<Variable> m_vars; // Stores the ranges of feasible region
+		MDR::Design m_performances; // Store the performance metrics
+												 // of the configuration
+		bool m_perfs_evaluated = false; // Stores whether the performances have
+										// been evaluated or not
+
+	public:
+		// Default constructor (constructs an empty object)
+		Config() {};
+
+		// Intended constructor
+		Config(const std::vector<Variable>& vars) { m_vars = vars; };
+
+		// Intended constructor (with performance metrics)
+		Config(const std::vector<Variable>& vars,
+			const MDR::Design& performances);
+
+		// Copy constructor (Currently Disabled)
+		//Config(const Config& ip_var) {};
+
+		// Set the performance metrics
+		void set_performances(const MDR::Design& performances);
+
+		// Get the performance metrics
+		MDR::Design get_performances() const { return m_performances; };
+
+		// Get the ranks
+		std::vector<size_t> get_ranks() const { return m_performances.get_ranks(); };
+
+		// Get the variables
+		std::vector<Variable> get_vars() const { return m_vars; };
+
+		// Return the size of the variables
+		size_t size_vars() const { return m_vars.size(); };
+
+		// Return whether the variable is feasible or not
+		bool is_feasible() const;
+
+		// Overload the index operator
+		Variable operator [](int i) const { return m_vars[i]; }
+		Variable& operator [](int i) { return m_vars[i]; }
+
+		//// Overload the index operator
+		//Variable operator [](int i) const { return m_vars[i]; }
+		//Variable& operator [](int i) { return m_vars[i]; }
+	};
+
+}
+
+#endif
---
+++ TabuSearch/src/Variable.cpp
@@ -0,0 +1,80 @@
+#include <vector>
+#include <string>
+#include <math.h>
+
+#include "../headers/Variable.h"
+
+namespace TS {
+
+	// Intended constructor for the Variable class
+	Variable::Variable(const bool& discrete, const std::vector<std::array<double, 2>>& feasible_regions,
+		const double& scale, const double& value, const double& stepsize,
+		const std::string& name) {
+
+		m_discrete = discrete;
+		m_feasible_regions = feasible_regions;
+		m_scale = scale;
+		m_val = value;
+		m_step = stepsize;
+		m_name = name;
+	}
+
+	// Check whether two variables are the same
+	bool Variable::same_variable(const Variable& other) const {
+
+		// Keep a tally of how many things are the same
+		bool same = true;
+
+		// Get the step size of the input variable
+		double stepsize = other.get_step();
+
+		if (m_discrete != other.get_discrete()) { same = false; }
+		if (m_name != other.get_name()) { same = false; }
+		if (std::abs(m_val - other.get_val()) > 1e-6) { same = false; }
+
+		// I did not check for the same feasible regions
+
+		return same;
+	};
+
+	// Check whether two variables are the relative to the step size of the next variable
+	bool Variable::same_variable_step(const Variable& other) const {
+
+		// Keep a tally of how many things are the same
+		bool same = true;
+
+		// Get the step size of the input variable
+		double stepsize = other.get_step();
+
+		if (m_discrete != other.get_discrete()) { same = false; }
+		if (m_name != other.get_name()) { same = false; }
+		if (std::abs(m_val - other.get_val()) > stepsize / 4) { same = false; }
+
+		// I did not check for the same feasible regions
+
+		return same;
+	};
+
+	// Check whether a value lies within the feasible region
+	bool Variable::is_feasible(size_t& feasible_idx) const {
+
+		feasible_idx = 0;
+
+		// Check whether the value lies within any of the feasible regions
+		for (size_t i = 0; i < m_feasible_regions.size(); i++) {
+			std::array<double, 2> current_region = m_feasible_regions[i];
+
+			if ((current_region[0] <= m_val) && (current_region[1] >= m_val)) {
+				// Give the feasible region index
+				feasible_idx = i;
+
+				// If the value is within a feasible region, return true
+				return true;
+			}
+		}
+
+		// If the value was in no feasible region, return false
+		return false;
+	}
+
+}
