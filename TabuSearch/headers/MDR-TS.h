21834c6578dc9a231ed35892e920b0a126241574
Calebsakhtar <calebsakhtar@gmail.com>
2022-02-09 20:29:39 +0000

    Add a header containing all of TS and MDR

---
+++ TabuSearch/headers/MDR-TS.h
@@ -0,0 +1,19 @@
+#ifndef TS_MDR_H
+#define TS_MDR_H
+
+// This header includes all needed TS and MDR Headers
+
+#include "Config.h"
+#include "MDROptimizer.h"
+#include "ObjectiveFunction.h"
+#include "ParetoMemory.h"
+#include "STM.h"
+#include "LTM.h"
+#include "Variable.h"
+
+#include "../../MDR-Test/MDR Test Project/headers/DesignClasses.h"
+#include "../../MDR-Test/MDR Test Project/headers/MDRFunctions.h"
+#include "../../MDR-Test/MDR Test Project/headers/ReadDesigns.h"
+
+
+#endif
