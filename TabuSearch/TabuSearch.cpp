ab870134b9ba58bf727ebee3036d37d1047ad9c3
Calebsakhtar <46966291+Calebsakhtar@users.noreply.github.com>
2022-05-24 11:14:53 +0200

    Enable off-design evaluation (#23)
    * Add a way to evaluate off-design aircraft
    
    * Change the main function to enable previous commit
    
    * Fix previous commit issue

--- TabuSearch/TabuSearch.cpp
+++ TabuSearch/TabuSearch.cpp
@@ -137,7 +137,98 @@ void passnum_test() {
 int main()
 {
 
-    const bool TS = false;
+    const bool TS = true;
+    const bool offdesign = true;
+
+    if (offdesign) {
+
+        // Initialize the vector containing the variables
+        std::vector<TS::Variable> vars;
+
+        // Create a variable for the range
+        std::array<double, 2> feas_reg_range = { 500, 2800 };
+        std::vector<std::array<double, 2>> feas_regs_range = { feas_reg_range };
+        const double start_range = 1400; // km
+        const double stepsize_range = 400; // km
+        const double stepsize_min_range = 100; // km
+        const std::string name_range = "Range (km)";
+        TS::Variable var_range(false, feas_regs_range, stepsize_min_range, start_range, stepsize_range,
+            name_range);
+
+        // Create a variable for the max. single engine power
+        std::array<double, 2> feas_reg_Pmax = { 1500, 3500 }; //kW
+        std::vector<std::array<double, 2>> feas_regs_Pmax = { feas_reg_Pmax };
+        const double start_Pmax = 2050;
+        const double stepsize_Pmax = 500;
+        const double stepsize_min_Pmax = 50;
+        const std::string name_Pmax = "Max. Single Engine Power (kW)";
+        TS::Variable var_Pmax(false, feas_regs_Pmax, stepsize_min_Pmax, start_Pmax, stepsize_Pmax,
+            name_Pmax);
+
+        // Create a variable for the Cruise Altitude
+        std::array<double, 2> feas_reg_h = { 4.500, 7.620 }; // km (15000 - 25000 ft)
+        std::vector<std::array<double, 2>> feas_regs_h = { feas_reg_h };
+        const double start_h = 6.100; // km (20000 ft)
+        const double stepsize_h = 0.620; // km
+        const double stepsize_min_h = 0.300; // km
+        const std::string name_h = "Cruise Altitude (km)";
+        TS::Variable var_h(false, feas_regs_h, stepsize_min_h, start_h, stepsize_h, name_h);
+
+        // Create a variable for the Cruise Mach Number
+        std::array<double, 2> feas_reg_M = { 0.4, 0.67 };
+        std::vector<std::array<double, 2>> feas_regs_M = { feas_reg_M };
+        const double start_M = 0.45;
+        const double stepsize_M = 0.2;
+        const double stepsize_min_M = 0.05;
+        const std::string name_M = "Cruise Mach Number";
+        TS::Variable var_M(false, feas_regs_M, stepsize_min_M, start_M, stepsize_M, name_M);
+
+        // Create a variable for the Hydrogen power fraction
+        std::array<double, 2> feas_reg_H2_Pfrac = { 0, 1 };
+        std::vector<std::array<double, 2>> feas_regs_H2_Pfrac = { feas_reg_H2_Pfrac };
+        const double start_H2_Pfrac = 0.5;
+        const double stepsize_H2_Pfrac = 0.4;
+        const double stepsize_min_H2_Pfrac = 0.05;
+        const std::string name_H2_Pfrac = "Hydrogen Power Fraction";
+        TS::Variable var_H2_Pfrac(false, feas_regs_H2_Pfrac, stepsize_min_H2_Pfrac, start_H2_Pfrac,
+            stepsize_H2_Pfrac, name_H2_Pfrac);
+
+        vars.push_back(var_range);
+        vars.push_back(var_Pmax);
+        vars.push_back(var_h);
+        vars.push_back(var_M);
+        vars.push_back(var_H2_Pfrac);
+
+        // Make the initial point from the variables
+        TS::Config initial_point(vars);
+
+        // Set up the simulation
+        printf("Setting up Simulation\n");
+
+        // Open Socket
+        const char* IP = "128.232.228.203";//"192.168.0.93";//"192.168.1.150";     //IP Address of computer running X-Plane
+        XPCSocket sock = openUDP(IP);
+        float tVal[1];
+        int tSize = 1;
+        if (getDREF(sock, "sim/test/test_float", tVal, &tSize) < 0)
+        {
+            printf("Error establishing connection. Unable to read data from X-Plane.");
+            return EXIT_FAILURE;
+        }
+        else {
+            printf("Initial connection successful.\n");
+        }
+
+        const double& desired_range = 1;
+        const double& desired_h = 6.100; // km
+        const double& desired_m = 0.45;
+
+        AircraftEval::evaluate_off_design_aircraft(initial_point, sock, 2810, desired_range,
+            desired_h, desired_m);
+
+        return 0;
+
+    }
 
     // Create a variable for the range
     std::array<double, 2> feas_reg_range = { 500, 2800 };
@@ -191,7 +282,7 @@ int main()
         const std::string name_H2_Pfrac = "Hydrogen Power Fraction";
         TS::Variable var_H2_Pfrac(false, feas_regs_H2_Pfrac, stepsize_min_H2_Pfrac, start_H2_Pfrac,
             stepsize_H2_Pfrac, name_H2_Pfrac);
-        
+
         vars.push_back(var_range);
         vars.push_back(var_Pmax);
         vars.push_back(var_h);
@@ -205,7 +296,7 @@ int main()
         MDR::DomRel first_layer(7, 3); // 3, 4
         MDR::DomRel second_layer(2, 8); // 0, 4
         MDR::DomRel third_layer(9, 2); // 1, 2
-        std::vector<MDR::DomRel> dom_rels = { first_layer, second_layer, third_layer}; //{ first_layer, second_layer, third_layer }
+        std::vector<MDR::DomRel> dom_rels = { first_layer, second_layer }; //{ first_layer, second_layer, third_layer }
 
         // Specify the TS Parameters
         size_t STM_size = 7;
@@ -221,7 +312,7 @@ int main()
         printf("Setting up Simulation\n");
 
         // Open Socket
-        const char* IP = "192.168.0.93";//"192.168.1.150";     //IP Address of computer running X-Plane
+        const char* IP = "128.232.228.203";//"192.168.0.93";//"192.168.1.150";     //IP Address of computer running X-Plane
         XPCSocket sock = openUDP(IP);
         float tVal[1];
         int tSize = 1;
@@ -253,7 +344,7 @@ int main()
         const size_t n = 1000;
         std::vector<TS::Config> configs;
 
-        for (size_t i = 0; i < n+1; i++) {
+        for (size_t i = 0; i < n + 1; i++) {
 
             // Initialize the vector containing the variables
             std::vector<TS::Variable> vars;
@@ -261,7 +352,7 @@ int main()
             // Create a variable for the Hydrogen power fraction
             std::array<double, 2> feas_reg_H2_Pfrac = { 0, 1 };
             std::vector<std::array<double, 2>> feas_regs_H2_Pfrac = { feas_reg_H2_Pfrac };
-            const double start_H2_Pfrac = 1;// static_cast<double>(i) * 1. / static_cast<double>(n);
+            const double start_H2_Pfrac = static_cast<double>(i) * 1. / static_cast<double>(n);
             const double stepsize_H2_Pfrac = 0.4;
             const double stepsize_min_H2_Pfrac = 0.05;
             const std::string name_H2_Pfrac = "Hydrogen Power Fraction";
@@ -286,7 +377,7 @@ int main()
         std::string filename = "AllPoints";
         printconfigs(configs, filename);
     }
-	
+
     // Mark the end of the program
     printf("---End Program---\n");
 
